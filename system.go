4c2062d82e79caaa0b93d3f12be289712bfe4f5b
李有鹏 <53730512@qq.com>
2019-06-25 21:57:42 +0800

    1

--- system.go
+++ system.go
@@ -13,11 +13,9 @@ const (
 	OSLinux
 )
 
-var (
-	_SetConsoleTitle uintptr
-)
 
 type sysST struct {
+	_SetConsoleTitle uintptr
 }
 
 func NewSys() *sysST {
@@ -31,12 +29,12 @@ func NewSys() *sysST {
 
 func (v *sysST) Init() bool {
 	if v.OS() == OSWindows {
-		kernel32, loadErr := syscall.LoadLibrary("kernel32.dll")
-		if loadErr != nil {
-			fmt.Println("loadErr", loadErr)
-		}
-		defer syscall.FreeLibrary(kernel32)
-		_SetConsoleTitle, _ = syscall.GetProcAddress(kernel32, "SetConsoleTitleW")
+		// kernel32, loadErr := syscall.LoadLibrary("kernel32.dll")
+		// if loadErr != nil {
+		// 	fmt.Println("loadErr", loadErr)
+		// }
+		// defer syscall.FreeLibrary(kernel32)
+		// v._SetConsoleTitle, _ = syscall.GetProcAddress(kernel32, "SetConsoleTitleW")
 	}
 
 	return true
@@ -56,13 +54,15 @@ func (v *sysST) OS() uint8 {
 }
 
 func (v *sysST) SetConsoleTitle(title string) int {
-	if v.OS() != OSWindows {
-		return 0
-	}
+	// if v.OS() != OSWindows {
+	// 	return 0
+	// }
 
-	ret, _, callErr := syscall.Syscall(_SetConsoleTitle, 1, uintptr(unsafe.Pointer(syscall.StringToUTF16Ptr(title))), 0, 0)
-	if callErr != 0 {
-		fmt.Println("callErr", callErr)
-	}
-	return int(ret)
+	// ret, _, callErr := syscall.Syscall(v._SetConsoleTitle, 1, uintptr(unsafe.Pointer(syscall.StringToUTF16Ptr(title))), 0, 0)
+	// if callErr != 0 {
+	// 	fmt.Println("callErr", callErr)
+	// }
+	// return int(ret)
+
+	return 0
 }
